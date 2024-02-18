bcda59af0b24d6e48d42edcd0381e1682f3ff9d3
Matheus <matheusteodorogarcia@gmail.com>
2024-02-18 15:50:22 -0300

    Organização dos estáticos em src/

---
+++ src/ts/main.ts
@@ -0,0 +1,5 @@
+import "../css/style.css";
+// import './images.ts'
+import '/src/img/slides/icone-cabelo.jpg'
+
+console.log("Hello World!");
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: { manifest: true },
+  build: {
+    manifest: true,
+    assetsInclude: ['images', 'favicon.ico'],
+  },
   base: process.env.mode === "production" ? "/static/" : "/",
   root: ".",
 });
