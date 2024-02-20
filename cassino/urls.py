09cc04c924d18f6b42430455940252cbd8cf50ec
Matheus <matheusteodorogarcia@gmail.com>
2024-02-19 22:54:37 -0300

    Adicionando imagens do header e slides, criando view home

--- cassino/urls.py
+++ cassino/urls.py
@@ -22,6 +22,8 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
   path('admin/', admin.site.urls),
+  path('', include('core.urls')),
+
   re_path(r'^static/(?P<path>.*)$', serve, {
 	  'document_root': settings.STATIC_ROOT
   }),    
