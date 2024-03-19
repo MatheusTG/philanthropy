9a15264222780b6275c7c488e8b37387939399b4
Matheus <matheusteodorogarcia@gmail.com>
2024-03-19 15:38:03 -0300

    Login e cadastro no header trocado por ícone de pódio

--- accounts/views.py
+++ accounts/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 
 def login(request):
   context = {
-    'login_container': True,
+
   }
 
   return render(request, 'core/home.html', context)
 
 def cadastro(request):
   context = {
-    'cadastro_container': True,
+
   }
 
   return render(request, 'core/home.html', context)
