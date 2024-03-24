9d2a7315f344f6afad26fc80787481d7227508e8
Matheus <matheusteodorogarcia@gmail.com>
2024-03-24 12:22:47 -0300

    Validando e salvando dados de cadastro

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,14 +5,36 @@ from django.contrib.auth.models import User
 
 
 class RegisterForm(UserCreationForm):
+  first_name = forms.CharField(
+    label='Nome',
+    help_text=None,
+  )
+ 
+  last_name = forms.CharField(
+    label='Sobrenome',
+    help_text='*opcional',
+    required=False,
+  )
+
   email = forms.EmailField(
-    widget=forms.TextInput(
+    widget=forms.EmailInput(
       attrs={
-        'type': 'email',
         'placeholder': 'email@email.com'
       }
     ),
-    label='E-mail'
+    label='E-mail',
+  )
+
+  password1 = forms.CharField(
+    widget=forms.PasswordInput(),
+    label='Senha',
+    help_text=None,
+  )
+
+  password2 = forms.CharField(
+    widget=forms.PasswordInput(),
+    label='Confirme sua senha',
+    help_text=None,
   )
 
   class Meta:
@@ -20,5 +42,25 @@ class RegisterForm(UserCreationForm):
     fields = (
       'first_name', 'last_name', 'email', 'password1', 'password2'
     )
+
+  def clean(self):
+    # Define o username igual ao email
+    cleaned_data = super().clean()
+    email = cleaned_data.get('email')
+    if email:
+      cleaned_data['username'] = email
+    return cleaned_data
   
-  
+  def clean_first_name(self):
+    first_name = self.cleaned_data.get('first_name')
+
+    if len(first_name) < 2:
+      self.add_error(
+        'first_name',
+        ValidationError(
+          'O nome precisa ter pelo menos 2 caracteres.',
+          code='invalid'
+        )
+      )
+    
+    return first_name
--- accounts/views.py
+++ accounts/views.py
@@ -5,9 +5,16 @@ from accounts.forms import RegisterForm
 
 def cadastro_user(request):
   if request.method == 'POST':
+    register_form = RegisterForm(request.POST)
+
     context = {
-      'register_form': RegisterForm(request.POST),
+      'register_form': register_form,
     }
+
+    if register_form.is_valid():
+      user = register_form.save(commit=False)
+      user.username = user.email
+      user.save()
 
     return render(request, 'core/home.html', context)
 
