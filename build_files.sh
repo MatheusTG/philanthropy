5db662931e6282fb850fa6adbde05d9329ab65d3
Matheus <matheusteodorogarcia@gmail.com>
2024-03-30 12:19:18 -0300

    Configurando projeto para o Vercel

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,8 @@
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
+echo "BUILD START" 
+python3.9 -m ensurepip 
+python3.9 -m pip install -r requirements.txt
+npm install
+npx tsc
+npx vite build
+python3.9 manage.py collectstatic --noinput --clear 
+echo "BUILD END"
